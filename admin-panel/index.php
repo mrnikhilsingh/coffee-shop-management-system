1c9f76eed7680fead3f1cb7c2d60b07160c71ba2
Nikhil <m.j882600@gmail.com>
2024-05-23 13:36:27 +0530

    show stats on index page from database

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -1,4 +1,5 @@
 <?php require "layouts/header.php"; ?>
+<?php require "../config/config.php"; ?>
 
 <?php 
 
@@ -6,6 +7,34 @@
 //he should not able to access index page
 if (!isset($_SESSION['admin_name'])) {
   header("Location: http://localhost/workspace/ns-coffee/admin-panel/admins/login.php");
+}
+
+// products
+$product_query = "SELECT COUNT(*) AS count FROM products";
+$product_result = mysqli_query($conn, $product_query) or die("Query Unsuccessful");
+if (mysqli_num_rows($product_result) > 0) {
+  $products_count = mysqli_fetch_assoc($product_result);
+}
+
+// orders
+$order_query = "SELECT COUNT(*) AS count FROM orders";
+$order_result = mysqli_query($conn, $order_query) or die("Query Unsuccessful");
+if (mysqli_num_rows($order_result) > 0) {
+  $orders_count = mysqli_fetch_assoc($order_result);
+}
+
+// bookings
+$booking_query = "SELECT COUNT(*) AS count FROM bookings";
+$booking_result = mysqli_query($conn, $booking_query) or die("Query Unsuccessful");
+if (mysqli_num_rows($booking_result) > 0) {
+  $bookings_count = mysqli_fetch_assoc($booking_result);
+}
+
+// admins
+$admin_query = "SELECT COUNT(*) AS count FROM admins";
+$admin_result = mysqli_query($conn, $admin_query) or die("Query Unsuccessful");
+if (mysqli_num_rows($admin_result) > 0) {
+  $admins_count = mysqli_fetch_assoc($admin_result);
 }
 
 ?>
@@ -16,7 +45,7 @@ if (!isset($_SESSION['admin_name'])) {
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Products</h5>
-          <p class="card-text">number of products: 8</p>
+          <p class="card-text">number of products: <?php echo $products_count['count']; ?></p>
         </div>
       </div>
     </div>
@@ -24,7 +53,7 @@ if (!isset($_SESSION['admin_name'])) {
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Orders</h5>
-          <p class="card-text">number of orders: 4</p>
+          <p class="card-text">number of orders: <?php echo $orders_count['count']; ?></p>
         </div>
       </div>
     </div>
@@ -32,7 +61,7 @@ if (!isset($_SESSION['admin_name'])) {
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Bookings</h5>
-          <p class="card-text">number of bookings: 4</p>
+          <p class="card-text">number of bookings: <?php echo $bookings_count['count']; ?></p>
         </div>
       </div>
     </div>
@@ -40,7 +69,7 @@ if (!isset($_SESSION['admin_name'])) {
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Admins</h5>
-          <p class="card-text">number of admins: 3</p>
+          <p class="card-text">number of admins: <?php echo $admins_count['count']; ?></p>
         </div>
       </div>
     </div>
